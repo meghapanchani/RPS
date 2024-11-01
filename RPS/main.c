0fb8472a620a11ae4f14dea556ef19858b9509bc
meghapanchani <mpanchani4117@conestogac.on.ca>
2024-11-01 16:14:44 -0400

    Test3 passed

--- RPS/main.c
+++ RPS/main.c
@@ -5,13 +5,22 @@ char* determineOutcome(const char* player1, const char* player2)
 {
     printf("Player1: %s, Player2: %s\n", player1, player2);
 
-
+    // Test 1
     if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) 
     {
         return "Player1";
     }
 
-    
+    // Test 2
+    if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Paper") == 0) {
+        return "Player2";
+    }
+
+    // Test 3
+    if (strcmp(player1, player2) == 0) {
+        return "Draw";
+    }
+
     return "Fail";
 }
 
